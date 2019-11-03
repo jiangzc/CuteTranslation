80391c6b06026e9c72e3eeaa9c14ae6dc1fa1348
jzc <985811440@qq.com>
2019-11-03 12:32:43 +0800

    add some info

--- src/picker.h
+++ src/picker.h
@@ -1,3 +1,25 @@
+/* -*- Mode: C++; indent-tabs-mode: nil; tab-width: 4 -*-
+ * -*- coding: utf-8 -*-
+ *
+ * Copyright (C) Ziqiang Xu
+ *
+ * Author:     Ziqiang Xu <ziqiang_xu@yeah.net>
+ * Maintainer: Ziqiang Xu <ziqiang_xu@yeah.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PICKER_H
 #define PICKER_H
 
