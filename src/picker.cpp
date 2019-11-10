9d523b00d210e91146bbb09a21ac54902a13e76a
jzc <985811440@qq.com>
2019-11-10 10:42:10 +0800

    update picker

--- src/picker.cpp
+++ src/picker.cpp
@@ -1,25 +1,3 @@
-/* -*- Mode: C++; indent-tabs-mode: nil; tab-width: 4 -*-
- * -*- coding: utf-8 -*-
- *
- * Copyright (C) Ziqiang Xu
- *
- * Author:     Ziqiang Xu <ziqiang_xu@yeah.net>
- * Maintainer: Ziqiang Xu <ziqiang_xu@yeah.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <QApplication>
 #include <QDebug>
 #include "picker.h"
@@ -30,10 +8,14 @@ Picker::Picker(QObject *parent) : QObject(parent)
 {
     clipboard = QApplication::clipboard();
     connect(clipboard, &QClipboard::selectionChanged, this, [=] {
-        // do not emit the signal right now, for browser
-
+        //
         text = clipboard->text(QClipboard::Selection);
-        if (!isPressed && text != "")
+
+        if (text == "")
+        {
+            clipboard->blockSignals(true);
+        }
+        else
         {
             CurrentWindowsPath = xdotool.getProcessPathByPID(xdotool.getActiveWindowPID());
             CurrentWindowsPath = CurrentWindowsPath.mid(1 + CurrentWindowsPath.lastIndexOf("/"));
@@ -46,20 +28,24 @@ Picker::Picker(QObject *parent) : QObject(parent)
 
 void Picker::buttonPressed()
 {
-    this->text = "";
-    this->isPressed = true;
+
 }
 
 void Picker::buttonReleased()
 {
-    this->isPressed = false;
-    // to get the selected text
-    if (text != "")
+
+
+    if (clipboard->signalsBlocked())
     {
+        qDebug() << "signalsBlocked";
+        text = clipboard->text(QClipboard::Selection);
+        clipboard->blockSignals(false);
+
         CurrentWindowsPath = xdotool.getProcessPathByPID(xdotool.getActiveWindowPID());
         CurrentWindowsPath = CurrentWindowsPath.mid(1 + CurrentWindowsPath.lastIndexOf("/"));
         Text = text;
         emit wordsPicked(text);
+
     }
 }
 
