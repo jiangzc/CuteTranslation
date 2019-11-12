4550e3c040edd5fc86449cfc562260618c5990d6
jzc <985811440@qq.com>
2019-11-12 18:39:33 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PRO_DIR=`dirname $0`
+if [ "$1" = "build" ]; then
+    INSTALL_DIR="`dirname $0`/build"
+else
+    INSTALL_DIR=/opt/CuteTranslation
+fi
+mkdir -p ${INSTALL_DIR}
+cp ${PRO_DIR}/python/*.py ${INSTALL_DIR}
+cp ${PRO_DIR}/template/* ${INSTALL_DIR}
+cp ${PRO_DIR}/build/CuteTranslation ${INSTALL_DIR}
+mkdir -p ~/.config/CuteTranslation
+cp ${PRO_DIR}/config.ini ~/.config/CuteTranslation
