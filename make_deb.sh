45bd1ee3e3f12e3485b635c903b785f6f5a1b9ce
jzc <985811440@qq.com>
2020-01-11 20:26:28 +0800

    add chown in make_deb.sh

--- make_deb.sh
+++ make_deb.sh
@@ -8,4 +8,5 @@ cp build/CuteTranslation deb/CuteTranslation/opt/CuteTranslation
 cp python/*.py deb/CuteTranslation/opt/CuteTranslation
 cp template/* deb/CuteTranslation/opt/CuteTranslation
 cd deb
+chown root.root -R ./CuteTranslation
 dpkg -b ./CuteTranslation ./CuteTranslation_v0.1.0_amd64.deb
