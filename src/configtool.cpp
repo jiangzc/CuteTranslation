a5a21d773154a98d1172ff5e1bc865db17d4bd21
jzc <985811440@qq.com>
2020-02-23 21:44:31 +0800

    use qApp->applicationVersion()

--- src/configtool.cpp
+++ src/configtool.cpp
@@ -10,10 +10,11 @@ ConfigTool *configTool;
 ConfigTool::ConfigTool()
 {
     // 必须文件夹
+    appDir.setPath(QCoreApplication::applicationDirPath());
+    dataDir.setPath(QDir::homePath() + "/.local/share/CuteTranslation");
     QDir::home().mkpath(dataDir.absolutePath());
     QDir::home().mkpath(QDir::homePath() + "/.config/autostart");
-    appDir.setPath(QCoreApplication::applicationDirPath());
-    dataDir.setPath(QDir::homePath() + "/.config/CuteTranslation");
+
 
     settings = new QSettings(dataDir.filePath("config.ini"), QSettings::IniFormat);
     Mode = GetMode(settings->value("/Picker/Mode", "all").toString().toLower());
@@ -43,9 +44,9 @@ ConfigTool::ConfigTool()
     // top-level key -> /General/Version
     QString version = settings->value("Version", "0.0.0").toString();
     TokenURL = settings->value("TokenURL", "http://67.216.199.87:5000/token").toString();
-    if (version != CUTETRANSLATION_VERSION)
+    if (version != qApp->applicationVersion())
     {
-        qWarning() << "程序和配置文件版本不匹配，程序版本：" << CUTETRANSLATION_VERSION << "，配置文件版本：" << version;
+        qWarning() << "程序和配置文件版本不匹配，程序版本：" << qApp->applicationVersion() << "，配置文件版本：" << version;
     }
 }
 
