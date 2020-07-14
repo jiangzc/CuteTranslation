69b49145c286b01370954dfb3a6de7f59d328397
jzc <985811440@qq.com>
2020-07-14 20:31:35 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ else
     INSTALL_DIR=/opt/CuteTranslation
 fi
 mkdir -p ${INSTALL_DIR}
-cp ${PRO_DIR}/python/*.py ${INSTALL_DIR}
 cp ${PRO_DIR}/template/* ${INSTALL_DIR}
 cp ${PRO_DIR}/build/CuteTranslation ${INSTALL_DIR}
-mkdir -p ~/.config/CuteTranslation
+mkdir -p ~/.local/share/CuteTranslation
 
